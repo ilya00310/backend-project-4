0e14286234c54b717e65308ea36546a01151883f
ilya <kotena1980@bk.ru>
2024-10-01 22:21:58 +0500

    correct makefile

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { WebSite } from './webSite.js';
 import { getURL } from './utils.js';
 
 export const getGeneralLogic = (link, pathDirectory, webSite = new WebSite()) => {
-  // console.log(pathDirectory)
   const pathNewFile = path.join(pathDirectory, getURL(link));
   const nameNewDir = getURL(link, '_files');
   return webSite.downloadDataWebSite(link, pathNewFile, nameNewDir, webSite)
--- src/logicPictures.js
+++ src/logicPictures.js
@@ -4,31 +4,30 @@ import * as cheerio from 'cheerio';
 import fs from 'fs';
 import path from 'path';
 import axios from 'axios';
-import { Console } from 'console';
 import { convertStr } from './utils.js';
 
 const { promises: fsp } = fs;
 
 export const getLogicPicturesDownload = (pathFile, nameNewDir, link) => {
   const pathNewDir = path.join(pathFile, '..', nameNewDir);
+
   return fsp.mkdir(pathNewDir)
     .then(() => fsp.readFile(pathFile, 'utf-8'))
     .then((data) => {
       const $ = cheerio.load(data);
       const dataPicturesFilter = Object.values($('body').find('img')).filter((item) => item.attribs);
       const promises = dataPicturesFilter.map((item) => {
-        const linkURL = new URL(link);
         const pathImg = path.join(new URL(link).host, item.attribs.src);
         const nameFilePicture = convertStr(pathImg, /\/|\.(?!png)/g);
+        const linkURL = new URL(link);
         const newURL = new URL(item.attribs.src, linkURL.origin);
-        // console.log(newURL, linkURL)
-        if (newURL.origin !== linkURL.origin) {
-          return Promise.resolve;
-        }
-        console.log(newURL.href)
+        // if () {
+        //   return Promise.resolve();
+        // }
+        // console.log(new URL(item.attribs.src, linkURL.origin))
         return axios.get(newURL.href, { responseType: 'utf-8' })
           .then((loadImg) => fsp.writeFile(path.join(pathNewDir, nameFilePicture), loadImg.data, 'utf-8'))
-          .finally(() => {
+          .then(() => {
             $(`img[src = "${item.attribs.src}"]`).attr('src', path.join(nameNewDir, nameFilePicture));
             return fsp.writeFile(pathFile, $.html(), 'utf-8')
               .then(() => Promise.resolve());
@@ -38,4 +37,3 @@ export const getLogicPicturesDownload = (pathFile, nameNewDir, link) => {
         .finally(() => Promise.resolve());
     });
 };
-// https://cdn2.hexlet.io/assets/partners_logos/tinkoff-23252dd2fdde061db6881b91a5c48da614c5aadb5961a5ff3ad356949de8c96b.svg
