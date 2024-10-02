8e340956eb02917bb334b36ce92895d74a4ba147
ilya <kotena1980@bk.ru>
2024-10-02 18:04:38 +0500

    last version

--- src/logicPictures.js
+++ src/logicPictures.js
@@ -7,6 +7,7 @@ import axios from 'axios';
 import { convertStr } from './utils.js';
 
 const { promises: fsp } = fs;
+const getPathImg = (url) => (path.extname(url.pathname) ? path.join(url.host, url.pathname) : `${path.join(url.host, url.pathname)}.png`);
 
 export const getLogicPicturesDownload = (pathFile, nameNewDir, link) => {
   const pathNewDir = path.join(pathFile, '..', nameNewDir);
@@ -17,15 +18,15 @@ export const getLogicPicturesDownload = (pathFile, nameNewDir, link) => {
       const $ = cheerio.load(data);
       const dataPicturesFilter = Object.values($('body').find('img')).filter((item) => item.attribs);
       const promises = dataPicturesFilter.map((item) => {
-        const pathImg = path.join(new URL(link).host, item.attribs.src);
-        const nameFilePicture = convertStr(pathImg, /\/|\.(?!png)/g);
         const linkURL = new URL(link);
         const newURL = new URL(item.attribs.src, linkURL.origin);
-        // if () {
+        const pathImg = getPathImg(newURL);
+        const nameFilePicture = convertStr(pathImg, /\/|\.(?=.*[.])/g);
+        // if (newURL.host !== linkURL.host) {
         //   return Promise.resolve();
         // }
-        // console.log(new URL(item.attribs.src, linkURL.origin))
-        return axios.get(newURL.href, { responseType: 'utf-8' })
+        console.log(newURL.href)
+        return axios.get(newURL.href, { responseType: 'arraybuffer' })
           .then((loadImg) => fsp.writeFile(path.join(pathNewDir, nameFilePicture), loadImg.data, 'utf-8'))
           .then(() => {
             $(`img[src = "${item.attribs.src}"]`).attr('src', path.join(nameNewDir, nameFilePicture));
