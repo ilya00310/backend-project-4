9a80ccc6f0152511a65a2451b53a1220ba6b1d1c
ilya <kotena1980@bk.ru>
2024-10-07 03:27:36 +0500

    add log err

--- src/logicOutputError.js
+++ src/logicOutputError.js
@@ -4,6 +4,7 @@ import fs from 'fs';
 const { promises: fsp } = fs;
 
 export const getOutputError = (error, pathNewFile, pathDirNewFile) => {
+  console.log(error);
   if (Object.hasOwn(error, 'config')) {
     const link = error.config.url;
     const { status } = error;
