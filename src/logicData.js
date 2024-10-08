04e977ee23a1f8e8aa212780623f38bfc8d27a47
ilya <kotena1980@bk.ru>
2024-10-08 13:05:27 +0500

    delet excess test

--- src/logicData.js
+++ src/logicData.js
@@ -10,18 +10,9 @@ export const getLogicDataDownload = (link, pathNewFile, pathDirNewFile) => {
   require('axios-debug-log');
   const axios = require('axios');
   return fsp.access(pathDirNewFile, fs.constants.R_OK)
-    .then(() => fsp.stat(pathDirNewFile))
-    .then((stat) => {
-      if (!stat.isDirectory()) {
-        throw new Error(`${pathDirNewFile} is file`);
-      }
-      return axios.get(link);
-    })
+    .then(() => axios.get(link))
     .then(({ data }) => {
       defaultDebug('dataFile %s ', data);
       return fsp.writeFile(pathNewFile, data, 'utf-8');
-    })
-    .catch((err) => {
-      throw new Error(err);
     });
 };
