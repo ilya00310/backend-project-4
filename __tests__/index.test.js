c97d1a594eaa0fab3452859389c9bd0c854d7016
ilya <kotena1980@bk.ru>
2024-10-13 00:19:42 +0500

    fix test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -14,16 +14,16 @@ nock.disableNetConnect();
 let currentPath;
 const link = 'https://ru.hexlet.io/courses';
 const nockDebug = debug('page-loader: nock.');
-const getNock = (pathData, pathExpectedFile, status = 200) => nock(/ru\.hexlet\.io/)
+const getNock = (pathData, pathExpectedFile, status = 200, encoding = 'utf-8') => nock(/ru\.hexlet\.io/)
   .persist()
   .get(pathData)
-  .replyWithFile(status, pathExpectedFile, { 'Content-Type': 'utf-8' }, nockDebug('request %s %s ', 'GET', link), nockDebug('status %d %s', 200, 'OK'));
+  .replyWithFile(status, pathExpectedFile, { 'Content-Type': encoding }, nockDebug('request %s %s ', 'GET', link), nockDebug('status %d %s', 200, 'OK'));
 beforeEach(async () => {
   currentPath = await fsp.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
 });
 beforeAll(async () => {
   getNock(/\/courses/, getFixturePath('file1.txt'));
-  getNock(/\/assets\/professions\/nodejs.png/, getFixturePath('file1.txt'));
+  getNock(/\/assets\/professions\/nodejs.png/, getFixturePath('assets/professions/nodejs.png'), 200, null);
   getNock(/\/assets\/application.css/, getFixturePath('file1.txt'));
   getNock(/\/packs\/js\/runtime.js/, getFixturePath('file1.txt'));
   getNock(/\/404/, getFixturePath('file1.txt'), 404);
@@ -34,7 +34,7 @@ describe('page-loader', () => {
     const expectedImg = await fsp.readFile(getFixturePath('assets/professions/nodejs.png'), null);
     const pathDownloadSite = await getGeneralLogic(link, currentPath);
     const regex = /\/tmp\/page-loader-X*\w*\/ru-hexlet-io-courses\.html/;
-    const pathFilePicture = '/tmp/page-loader-XXXXXXxAoQtp/ru-hexlet-io-courses_files/ru-hexlet-io-assets-professions-nodejs.png';
+    const pathFilePicture = path.join(pathDownloadSite, '..', 'ru-hexlet-io-courses_files/ru-hexlet-io-assets-professions-nodejs.png');
     const afterHTML = await fsp.readFile(getFixturePath('file2.txt'), 'utf-8');
     const newFile = await fsp.readFile(pathDownloadSite, 'utf-8');
     expect(await fsp.stat(pathFilePicture)).not.toBeUndefined();
